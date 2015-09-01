4f1682c78df42cf5095a236528872f8ce031a672
I Chen Wu <steven.wu@concur.com>
2015-09-01 15:14:22 -0700

    fixed dummy data path

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(cookieParser());
 
-fs.readFile('./app/js/utils/APISample.json', 'utf8', function(err, data) {
+fs.readFile('APISample.json', 'utf8', function(err, data) {
     if (err) throw err;
     APISample = JSON.parse(data);
 });
